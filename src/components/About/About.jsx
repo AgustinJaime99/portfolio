ee43fd15d11ae227dc48a7723921001c618b0149
AgustinJaime99 <71357291+AgustinJaime99@users.noreply.github.com>
2021-04-23 23:15:26 -0300

    update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -38,10 +38,16 @@ const About = () => {
             <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
               <div className="about-wrapper__info">
                 <p className="about-wrapper__info-text">
-                Desarrollador Fullstack JavaScript, apasionado por el Front-end. Realicé un bootcamp de mas de 800 hs. de estudio intensivo que me preparó para desempeñarme como desarrolador Fullstack, dentro del curso aprendi desde lo basico hasta desarrollar una aplicacion en equipo.
+                  Desarrollador Fullstack JavaScript, apasionado por el Front-end. Realicé un
+                  bootcamp de más de 800 hs. de estudio intensivo que me preparó para desempeñarme
+                  como desarrollador Fullstack. Dentro del curso aprendí desde lo básico hasta
+                  desarrollar una aplicación en equipo.
                 </p>
                 <p className="about-wrapper__info-text">
-                    Me inicie en el mundo del desarrollo hace relativamente poco, quede fascinado con lo que podia crear solo escribiendo. Hoy busco oportunidades que ayuden a mi desarrollo personal. Soy una persona proactiva, siempre dispuesta, respetuosa y por sobretodo con ganas de trabajar en equipo.
+                  Me inicie en el mundo del desarrollo hace relativamente poco. Quede fascinado con
+                  lo que podía crear solo escribiendo. Hoy busco oportunidades que ayuden a mi
+                  desarrollo personal. Soy una persona proactiva, con buena disposición siempre
+                  dispuesta, respetuosa y por sobre todo con ganas de trabajar en equipo.
                 </p>
                 {resume && (
                   <span className="d-flex mt-3">
@@ -55,16 +61,18 @@ const About = () => {
                     </a>
                   </span>
                 )}
-                {cv && (<span className="d-flex mt-3">
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="cta-btn cta-btn--resume"
-                  href= {cv}
-                >
-                  Descargar CV
-                </a>
-                </span>)}
+                {cv && (
+                  <span className="d-flex mt-3">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="cta-btn cta-btn--resume"
+                      href={cv}
+                    >
+                      Descargar CV
+                    </a>
+                  </span>
+                )}
               </div>
             </Fade>
           </Col>
